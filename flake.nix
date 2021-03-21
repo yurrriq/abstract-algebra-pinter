d7e53493db8615ace31f3839d9b5e8d4545d23a0
Eric Bailey <eric@ericb.me>
2021-03-21 00:03:24 -0500

    nix flake init

---
+++ flake.nix
@@ -0,0 +1,90 @@
+{
+  description = ''Exercises from "A Book of Abstract Algebra"'';
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-20.09";
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        overlays = [
+          self.overlay
+        ];
+      };
+      src = pkgs.nix-gitignore.gitignoreSource [ ".git/" ] ./.;
+    in
+    {
+      devShell.x86_64-linux = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          gitAndTools.pre-commit
+          gnumake
+          haskellPackages.ormolu
+          nixpkgs-fmt
+          pythonPackages.pywatchman
+          # TODO: sagemath
+          xelatex
+        ] ++ self.packages.x86_64-linux.pinter.env.nativeBuildInputs;
+      };
+
+
+      overlay = final: prev: {
+        xelatex = prev.texlive.combine {
+          inherit (prev.texlive) scheme-small
+            amsrefs
+            braket
+            catchfile
+            datatool
+            datetime
+            ebproof
+            enumitem
+            glossaries
+            glossaries-extra
+            hyperref
+            latexmk
+            mfirstuc
+            tkz-base
+            # tkz-berge
+            # tkz-graph
+            todonotes
+            tracklang
+            xetex
+            xfor
+            xindy
+            xstring
+            ;
+        };
+      };
+
+      packages.x86_64-linux = {
+        abstract-algebra-pinter =
+          pkgs.stdenv.mkDerivation rec {
+            pname = "abstract-algebra-pinter";
+            version = builtins.readFile ./VERSION;
+            inherit src;
+
+            nativeBuildInputs = with pkgs; [
+              xelatex
+            ];
+
+            makeFlags = [
+              "PREFIX=${placeholder "out"}"
+            ];
+
+            meta = with pkgs.lib; {
+              description = ''Exercises from "A Book of Abstract Algebra"'';
+              longDescription = ''
+                Working through exercises in "A Book of Abstract Algebra"
+                by Charles C. Pinter.
+              '';
+              homepage = "https://github.com/yurrriq/abstract-algebra-pinter";
+              license = licenses.unlicense;
+              maintainers = with maintainers; [ yurrriq ];
+              platforms = platforms.all;
+            };
+          };
+        pinter = pkgs.haskellPackages.callCabal2nix "pinter" src { };
+      };
+
+      defaultPackage.x86_64-linux = self.packages.x86_64-linux.abstract-algebra-pinter;
+    };
+}
--- haskell.nix
+++
@@ -1,5 +0,0 @@
-{ pkgs ? import ./nix }:
-let
-  src = pkgs.nix-gitignore.gitignoreSource [ ] ./.;
-in
-pkgs.haskellPackages.callCabal2nix "pinter" src { }
